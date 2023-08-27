ba53093db36d5c467ba64d700293f8591aace38b
pgbloglace <pgbloglace@gmail.com>
2023-08-27 16:01:47 +0200

    H

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -20,11 +20,9 @@ function openCity(evt, cityName) {
 }
 
 $(document).ready(function(){
-  /*
   $('#en').show();
   $('#sp').hide();
   $('#ge').hide();
-  */
   $('#switch-lang-en').click(function() {
     $('#en').show();
     $('#sp').hide();
